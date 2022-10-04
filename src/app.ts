3db316f6675871c3cbe226456a2423f8996eb321
Venkatesh Subramanian <venkatsmani93@gmail.com>
2022-10-04 11:35:12 +0530

    Create basic express "GET"

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log(123);
+import express from 'express';
+
+const port: number = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(port, ()=> console.log(`Listening on port:${port}`));
