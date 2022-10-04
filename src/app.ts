8c3a4760a3f66e11a3f9d434b8c54dbd4ee59b41
Venkatesh Subramanian <venkatsmani93@gmail.com>
2022-10-04 18:43:53 +0530

    Enhanced app with Logging middleware and API headers

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,9 @@ import express, {
 import http from "http";
 import mongoose from "mongoose";
 import createHttpError from "http-errors";
+
 import { config } from "./config/config";
+import Logging from "./library/Logging";
 
 const port: number = config.server.port;
 const app: Application = express();
@@ -17,29 +19,76 @@ const app: Application = express();
 mongoose
   .connect(config.mongo.url, { retryWrites: true, w: "majority" })
   .then(() => {
-    console.log("mongo connected");
+    Logging.info("Mongo Connected");
+    startServer();
   })
   .catch((error) => {
-    console.log(error);
+    Logging.error("Error connecting to Mongo: ");
+    Logging.error(error);
   });
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello");
-});
+/** Start the server with Logging middleware */
+const startServer = () => {
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    /** Log the request */
+    Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}]`);
 
-// Middleware for error handling
-app.use((req: Request, res: Response, next: NextFunction) => {
-  next(new createHttpError.NotFound());
-});
+    res.on("finish", () => {
+      /** Log the response */
+      Logging.info(
+        `Incoming -> Method: [${req.method}] - Url: [${req.url}] - Status: [${res.statusCode}]`
+      );
+    });
 
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send({
-    status: err.status || 500,
-    message: err.message,
+    next();
   });
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  /** API Rules */
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, POST, PATCH, DELETE, GET"
+      );
+      return res.status(200).json({});
+    }
+
+    next();
+  });
+
+  /** Routes */
+  app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Hello!" });
+  });
+
+  // Middleware for error handling
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    const error = new Error("Not found");
+    Logging.error(error);
+    next(new createHttpError.NotFound());
+  });
+
+  const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    Logging.error(err);
+    res.status(err.status || 500);
+    res.send({
+      status: err.status || 500,
+      message: err.message,
+    });
+  };
+
+  app.use(errorHandler);
+
+  http
+    .createServer(app)
+    .listen(port, () => Logging.info(`Server is running on port ${port}`));
 };
-
-app.use(errorHandler);
-
-app.listen(port, () => console.log(`Listening on port:${port}`));
