977c3d13dc8f619fb0bb684891da7db7d702c472
Venkatesh Subramanian <venkatsmani93@gmail.com>
2022-10-04 11:59:21 +0530

    Created ts with Typings, Error handler and env configs

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
-import express from 'express';
+import express, {Application, ErrorRequestHandler, NextFunction, Request, Response} from 'express';
+import createHttpError from 'http-errors';
+import {config} from 'dotenv';
 
-const port: number = 3000;
-const app = express();
+config();
 
-app.get('/', (req, res) => {
+const port: number = Number(process.env.PORT) || 3000;
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
     res.send("Hello");
 });
+
+
+// Middleware for error handling
+app.use((req: Request, res: Response, next: NextFunction) => {
+    next(new createHttpError.NotFound());
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send({
+        status: err.status || 500,
+        message: err.message
+    })
+};
+
+app.use(errorHandler);
 
 app.listen(port, ()=> console.log(`Listening on port:${port}`));
